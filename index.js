b1cf474405cb44c582f3c31b56fa1f5e8e893c03
shakurbol <bolakokbol@gmail.com>
2023-10-02 07:54:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    const textElement = document.getElementById('text');
+  
+    
+    if (textElement) {
+      textElement.textContent = 'This is really cool!';
+    } else {
+      console.error('Element with id "text" not found.');
+    }
+  });
+  
